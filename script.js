7607a5ea0c535059e71c1d0704999c27bc28f2be
PhyllisJu <kj234@cornell.edu>
2022-06-29 16:49:37 -0500

    Add: a random restaurant generator

---
+++ script.js
@@ -0,0 +1,31 @@
+const answers = [
+  "Olive Garden",
+  "Pho Station",
+  "Oozo Ramen",
+  "Kungfu BBQ",
+  "Kungfu Tea",
+  "Kungfu Station",
+  "PuYueXiang",
+  "DongHai YuCun",
+  "Szechuan Hotpot",
+  "Star Karaoke",
+  "Szechuan Restaurant",
+  "MaiXiangCun",
+  "Yogi",
+];
+
+const count = answers.length;
+const viewBtn = document.getElementById("answer");
+const resetBtn = document.getElementById("reset");
+let answer = "View the Answer";
+viewBtn.innerHTML = answer;
+
+viewBtn.addEventListener("click", function () {
+  let index = Math.floor(Math.random() * count);
+  answer = answers[index];
+  viewBtn.innerHTML = answer;
+});
+
+resetBtn.addEventListener("click", function () {
+  viewBtn.innerHTML = "View the Answer";
+});
